85d5d03238f8d131250cd026d3ac82d2688b1e8c
Dina <sergidka@gmail.com>
2018-11-29 13:36:44 +0300

    Added PhotoGallery.Common project - created AppSettings for the access to the application settings;

---
+++ PhotoGallery.Common/Settings/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace PhotoGallery.Common.Settings
+{
+    public class AppSettings
+    {
+        public string DbConnectionString { get; set; }
+
+        public string JwtBearerKey { get; set; }
+
+        public int JwtBearerLifeTime { get; set; }
+    }
+}
