9c915135e538d0125c3e6af29d6a30d3b1b7a860
Dina <sergidka@gmail.com>
2019-02-03 23:24:55 +0300

    Updated styles for edit photo/album/login data pages: - Moved common styles to styles.scss common file; - Increased card width and description field height;

--- PhotoGallery/ClientApp/src/app/components/login/login.component.ts
+++ PhotoGallery/ClientApp/src/app/components/login/login.component.ts
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
           this.router.navigate(['/']);
         },
         error => {
-          console.log(error);
+          this.loading = false;
         }
       )
   }
