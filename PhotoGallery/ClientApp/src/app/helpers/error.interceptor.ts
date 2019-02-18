2ec20a5850b028d0373e5a93fb5de8e111517a0d
Dina <sergidka@gmail.com>
2019-02-18 14:19:52 +0300

    Added catching of unauthorized server error

--- PhotoGallery/ClientApp/src/app/helpers/error.interceptor.ts
+++ PhotoGallery/ClientApp/src/app/helpers/error.interceptor.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { AlertService } from 'src/app/services/alert.service';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/services/alert.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   constructor(
-    private alertService: AlertService
+    private alertService: AlertService,
+    private authenticationService: AuthenticationService,
+    private router: Router
   ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -16,6 +20,11 @@ export class ErrorInterceptor implements HttpInterceptor {
         catchError(err => {
           if (err.status === 400) {
             this.alertService.alert.next(err.error);
+          }
+
+          if (err.status === 401) {
+            this.authenticationService.logout();
+            this.router.navigate(['/login']);
           }
 
           return throwError(err);
