069c895722cbfcc320a0b50d62d66ddfc595e3c1
Dina <sergidka@gmail.com>
2019-02-18 14:15:14 +0300

    Moved setting of display name and mailto link to ngOnInit method

--- PhotoGallery/ClientApp/src/app/components/user-card/user-card.component.ts
+++ PhotoGallery/ClientApp/src/app/components/user-card/user-card.component.ts
@@ -7,7 +7,7 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./user-card.component.scss']
 })
 
-export class UserCardComponent {
+export class UserCardComponent implements OnInit {
   @Input() user: User;
   @Input() isCurrentUser: boolean;
 
@@ -16,20 +16,8 @@ export class UserCardComponent {
 
   constructor() { }
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.updateDisplayName();
-    this.updateMailToLink();
-  }
-
-  updateDisplayName() {
-    if (this.user) {
-      this.displayName = this.user.firstName + ' ' + this.user.lastName;
-    }
-  }
-
-  updateMailToLink() {
-    if (this.user) {
-      this.mailToLink = 'mailto:' + this.user.email;
-    }
+  ngOnInit(){
+    this.displayName = this.user.firstName + ' ' + this.user.lastName;
+    this.mailToLink = 'mailto:' + this.user.email;
   }
 }
